eb71d3327a62c8d6da4b643583c18341a3af9f76
ousttrue <ousttrue@gmail.com>
2020-09-07 23:39:48 +0900

    ~RawMode

--- termgrid/rawmode.cpp
+++ termgrid/rawmode.cpp
@@ -19,7 +19,16 @@ RawMode::RawMode(int tty) : m_tty(tty)
 
 RawMode::~RawMode()
 {
+#if 1
+    termios ioval;
+    tcgetattr(m_tty, &ioval);
+    ioval.c_lflag |= TTY_MODE;
+    ioval.c_iflag |= (IXON | IXOFF);
+    ioval.c_cc[VMIN] = 4;
+    while (tcsetattr(m_tty, TCSANOW, &ioval) == -1)
+#else
     while (tcsetattr(m_tty, TCSANOW, &m_ioval) == -1)
+#endif
     {
         if (errno == EINTR || errno == EAGAIN)
             continue;
